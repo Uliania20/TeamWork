ba537c2af2acdd2f46c10c2982acada03a959405
ritoru0 <tamaesan12@gmail.com>
2024-11-22 12:13:00 +0300

    commit Rita

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String developer1 = "Разработчик 1: Привет, я работаю над задачей.";
         System.out.println("Создание ветки и Работа с веткой uu");
+        System.out.println("изменение");
         System.out.println(developer1);
     }
 }
