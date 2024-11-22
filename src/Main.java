35e25b3903b6c50a6c4b507c4a69f3173bd34c8b
Uliania <ulianasobotovich@yandex.by>
2024-11-22 10:53:55 +0300

    commit uliania

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String developer1 = "Разработчик 1: Привет, я работаю над задачей.";
+        System.out.println(developer1);
+    }
+}
