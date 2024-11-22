34139540c016b5efd74a06ec623101fd9fcb6275
Uliania <ulianasobotovich@yandex.by>
2024-11-22 11:40:04 +0300

    merge

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String developer1 = "Разработчик 1: Привет, я работаю над задачей.";
+        System.out.println("Создание ветки");
         System.out.println(developer1);
     }
 }
